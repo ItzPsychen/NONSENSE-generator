c5f28b4445a14ce1ac4119db562b510e0bbd33cb
federicomaniglio <fmaniglio@itisrossi.vi.it>
2025-05-03 10:27:03 +0200

    Add initial Maven project setup with basic structure
    Introduce a new Maven-based project with a simple "Hello World" application in `App.java` and corresponding unit tests in `AppTest.java`. Includes a `.gitignore` for common IDEs and OS-generated files, a `pom.xml` for project dependencies, and initial project configuration.

---
+++ src/main/java/unipd/edids/App.java
@@ -0,0 +1,13 @@
+package unipd.edids;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
